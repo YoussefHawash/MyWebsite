ec286f260bfad6631580ff1e4f588036bde314ea
Youssef Medhat <ymbusinesses@gmail.com>
2021-10-05 09:47:24 +0200

    .

--- main/views.py
+++ main/views.py
@@ -10,27 +10,27 @@ def visitsCounter(mywebsite):
         mywebsite.save()
 
 def Home(request):
-    #  mywebsite = website.objects.get(pk=1)
-    #  owner = creator.objects.get(pk=1)
-    #  projects = project.objects.all()
-    #  services = service.objects.all()
-    #  bios = {
-    #     'firstbio': bio.objects.all()[0],
-    #     'secondbio': bio.objects.all()[1],
-    #       'thirdbio': bio.objects.all()[2]
-    #      }
-    #  if settings.DEBUG ==False:
-    #   visitsCounter(mywebsite)
-    #  context = {
-    #     'views': mywebsite.views,
-    #     'creator': owner,
-    #     'projects': projects,
-    #     'firstbio': bios['firstbio'],
-    #     'secondbio': bios['secondbio'],
-    #     'thirdbio': bios['thirdbio'],
-    #     'services':services,
-    #  }
-     return render(request, 'main/home.html')
+     mywebsite = website.objects.get(pk=1)
+     owner = creator.objects.get(pk=1)
+     projects = project.objects.all()
+     services = service.objects.all()
+     bios = {
+        'firstbio': bio.objects.all()[0],
+        'secondbio': bio.objects.all()[1],
+          'thirdbio': bio.objects.all()[2]
+         }
+     if settings.DEBUG ==False:
+      visitsCounter(mywebsite)
+     context = {
+        'views': mywebsite.views,
+        'creator': owner,
+        'projects': projects,
+        'firstbio': bios['firstbio'],
+        'secondbio': bios['secondbio'],
+        'thirdbio': bios['thirdbio'],
+        'services':services,
+     }
+     return render(request, 'main/home.html',  context)
 
 
 def profile(request):
