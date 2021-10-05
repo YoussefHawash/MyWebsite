2b3868c5ee34d78c5b0bc4bc0acb070580abfce6
Youssef Medhat <ymbusinesses@gmail.com>
2021-10-05 09:43:20 +0200

    .

--- usefsite/settings.py
+++ usefsite/settings.py
@@ -32,9 +32,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'main',
     'mathfilters',
     "taggit",
-    'main',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
