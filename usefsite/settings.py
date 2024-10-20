0c6f738f8e441f5ba684e16e793306e47ada72ea
Youssef <ymbusinesses@gmail.com>
2024-10-20 15:10:56 +0300

    env

--- usefsite/settings.py
+++ usefsite/settings.py
@@ -24,7 +24,7 @@ env = environ.Env(
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = "django-insecure-4*(s+mnkt8)h87no5r!tc8wlhif0*o2=gn8!w8uw-bf0175bs)"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
