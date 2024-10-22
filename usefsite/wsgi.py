0f47d0e333df2cd0f08485203b8814f831dad141
Youssef <ymbusinesses@gmail.com>
2024-10-22 06:25:37 +0300

    ruin

--- usefsite/wsgi.py
+++ usefsite/wsgi.py
@@ -14,3 +14,25 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'usefsite.settings')
 
 application = get_wsgi_application()
+[program:gunicorn]
+
+directory=/home/ubuntu/Portofolio_Website
+
+command=/home/ubuntu/Portofolio_Website/venv/bin/gunicorn --workers 3 --bind unix:/home/ubuntu/Portofolio_Website/app.sock usefsite.wsgi:application  
+
+autostart=true
+
+autorestart=true
+
+stderr_logfile=/var/log/gunicorn/gunicorn.err.log
+
+stdout_logfile=/var/log/gunicorn/gunicorn.out.log
+
+
+
+[group:guni]
+
+programs:gunicorn
+
+
+Sample django.conf:
