a6d20ace3ae2df1e79351e0a93ab223dfb137fd9
Youssef <ymbusinesses@gmail.com>
2024-11-09 04:11:37 +0200

    Pfp and static to s3

--- usefsite/settings.py
+++ usefsite/settings.py
@@ -1,24 +1,26 @@
-from pathlib import Path
-import djongo
 import os
+from pathlib import Path
 from dotenv import load_dotenv
+
 load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
+
 SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG') == 'True'
-ALLOWED_HOSTS =  os.getenv('ALLOWED_HOSTS').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
+
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY =  os.getenv('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME =  os.getenv('AWS_STORAGE_BUCKET_NAME')
-AWS_S3_REGION_NAME =  os.getenv('AWS_S3_REGION_NAME')
-AWS_S3_CUSTOM_DOMAIN =  os.getenv('AWS_S3_CUSTOM_DOMAIN')
-AWS_S3_FILE_OVERWRITE =  os.getenv('AWS_S3_FILE_OVERWRITE')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME')
+AWS_S3_CUSTOM_DOMAIN = os.getenv('AWS_S3_CUSTOM_DOMAIN')
 
 AUTH_USER_MODEL = 'main.CustomUser'  
+
 INSTALLED_APPS = [
     'main',
     'mathfilters',
-    "taggit",
+    'taggit',
     'storages',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -60,33 +62,40 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'usefsite.wsgi.application'
+
 DATABASES = {
     'default': {
         'ENGINE': 'djongo',
-        'NAME': os.getenv('DATABASE_NAME'),  # Replace with your MongoDB database name
+        'NAME': os.getenv('DATABASE_NAME'),
         'ENFORCE_SCHEMA': False,
         'CLIENT': {
-            'host': os.getenv('DATABASE_HOST'),  # Replace with your MongoDB host
-            'username':os.getenv('DATABASE_USER'),  
-            'password': os.getenv('DATABASE_PASSWORD'),  
-            'authSource': 'admin',   
-            'authMechanism': 'SCRAM-SHA-1',  
+            'host': os.getenv('DATABASE_HOST'),
+            'username': os.getenv('DATABASE_USER'),
+            'password': os.getenv('DATABASE_PASSWORD'),
+            'authSource': 'admin',
+            'authMechanism': 'SCRAM-SHA-1',
         }
     }
 }
 
 
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'  
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'  
 
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_S3_FILE_OVERWRITE = os.getenv('AWS_S3_FILE_OVERWRITE', 'True') == 'True'
+AWS_DEFAULT_ACL = None
 
-STORAGES = {
-    "default": {
-        "BACKEND": "storages.backends.s3boto3.S3StaticStorage",
-    },
-    "staticfiles": {
-        "BACKEND": "storages.backends.s3boto3.S3StaticStorage",
-    },
-}
+# Static and Media URLs
+STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
+
+# Static files directory
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+# Collect static will collect all static files from apps and STATICFILES_DIRS into S3
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -102,27 +111,14 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-SESSION_ENGINE = 'django.contrib.sessions.backends.cached_db'  
-SESSION_COOKIE_AGE = 86400  
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-STATIC_URL = '/static/'
-
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
+SESSION_ENGINE = 'django.contrib.sessions.backends.cached_db'
+SESSION_COOKIE_AGE = 86400
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
